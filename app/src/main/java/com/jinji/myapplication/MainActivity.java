ef5c478f3a0333c01f96637e53351531bf2ba4d6
mh <mh199242@163.com>
2017-03-27 18:09:13 +0800

    第si次提交

--- app/src/main/java/com/jinji/myapplication/MainActivity.java
+++ app/src/main/java/com/jinji/myapplication/MainActivity.java
@@ -1,11 +1,9 @@
 package com.jinji.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Button;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
